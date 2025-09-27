ede6d3ccaa86ff2731127840cdcd2259eba77a05
mesimeno <ju252722@ucf.edu>
2025-09-27 16:57:08 -0400

    Configure database connection to Supabase

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,6 +17,20 @@ import os
 
 load_dotenv()
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
+    }
+}
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
