7fc6f85e8be316a63c5fca4f2e6afac51f52a695
mesimeno <ju252722@ucf.edu>
2025-09-28 03:52:52 -0400

    Initial homepage route
    Also configured secret key in the .env file

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^c%ipwde&!(%ynhq)c%ue0u)u+spb-l$@rnmus$@$g#_6-84p5'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
